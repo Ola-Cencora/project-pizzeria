e692ac60ce7e239664bbc32e18e1ed9e76da73d4
Ola <cencora.aleksandra@gmail.com>
2023-06-29 12:15:23 +0200

    Add event listeners to initActions

--- src/js/script.js
+++ src/js/script.js
@@ -207,9 +207,10 @@ const select = {
   class AmountWidget{
     constructor(element){
       const thisWidget = this;
-
+      
       thisWidget.getElements(element);
       thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
 
       console.log('AmountWidget: ', thisWidget);
       console.log('constructor arguments: ', element);
@@ -235,6 +236,24 @@ const select = {
       }
 
       thisWidget.input.value = thisWidget.value;
+    }
+
+    initActions(){
+      const thisWidget = this;
+      
+      thisWidget.input.addEventListener('change', function(){
+        thisWidget.setValue(thisWidget.input.value);        
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
     }
   }
 
